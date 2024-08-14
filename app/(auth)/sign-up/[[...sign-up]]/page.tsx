ae378b5587e9fdc6f707fd0a80fba9b8d62ec68e
rozer <shubhamsinha07084@gmail.com>
2024-08-14 05:32:23 +0530

    code-routes [signup+signin]

---
+++ app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -0,0 +1,8 @@
+// this is the catch all routes where all routes are being catched
+//  related to sign up process
+
+import { SignUp } from "@clerk/nextjs";
+
+export default function Page() {
+  return <SignUp />;
+}
